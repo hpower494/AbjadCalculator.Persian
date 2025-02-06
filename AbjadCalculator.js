5d678176e615ede86ab80bec70ab8d82cb769376
hpower494 <hosseinranjbardounchali@gmail.com>
2025-02-06 22:32:31 +0330

    Update AbjadCalculator.js

--- AbjadCalculator.js
+++ AbjadCalculator.js
@@ -34,14 +34,67 @@ if (this.indexOf('ذ') > -1){var ذ = this.split("ذ").join("700").match(/(700)/
 if (this.indexOf('ض') > -1){var ض = this.split("ض").join("800").match(/(800)/gim).map(Number).reduce(getSum)}else{ض=0}
 if (this.indexOf('ظ') > -1){var ظ = this.split("ظ").join("900").match(/(900)/gim).map(Number).reduce(getSum)}else{ظ=0}
 if (this.indexOf('غ') > -1){var غ = this.split("غ").join("1000").match(/(1000)/gim).map(Number).reduce(getSum)}else{غ=0}
+if (this.indexOf('A') > -1){var A = this.split("A").join("1").match(/(1)/gim).map(Number).reduce(getSum)}else{A=0}
+if (this.indexOf('a') > -1){var a = this.split("a").join("1").match(/(1)/gim).map(Number).reduce(getSum)}else{a=0}
+if (this.indexOf('B') > -1){var B = this.split("B").join("2").match(/(2)/gim).map(Number).reduce(getSum)}else{B=0}
+if (this.indexOf('b') > -1){var b = this.split("b").join("2").match(/(2)/gim).map(Number).reduce(getSum)}else{b=0}
+if (this.indexOf('C') > -1){var C = this.split("C").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{C=0}
+if (this.indexOf('c') > -1){var c = this.split("c").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{c=0}
+if (this.indexOf('D') > -1){var D = this.split("D").join("4").match(/(4)/gim).map(Number).reduce(getSum)}else{D=0}
+if (this.indexOf('d') > -1){var d = this.split("d").join("4").match(/(4)/gim).map(Number).reduce(getSum)}else{d=0}
+if (this.indexOf('E') > -1){var E = this.split("E").join("5").match(/(5)/gim).map(Number).reduce(getSum)}else{E=0}
+if (this.indexOf('e') > -1){var e = this.split("e").join("5").match(/(5)/gim).map(Number).reduce(getSum)}else{e=0}
+if (this.indexOf('F') > -1){var F = this.split("F").join("80").match(/(80)/gim).map(Number).reduce(getSum)}else{F=0}
+if (this.indexOf('f') > -1){var f = this.split("f").join("80").match(/(80)/gim).map(Number).reduce(getSum)}else{f=0}
+if (this.indexOf('G') > -1){var G = this.split("G").join("120").match(/(120)/gim).map(Number).reduce(getSum)}else{G=0}
+if (this.indexOf('g') > -1){var g = this.split("g").join("120").match(/(120)/gim).map(Number).reduce(getSum)}else{g=0}
+if (this.indexOf('H') > -1){var H = this.split("H").join("8").match(/(8)/gim).map(Number).reduce(getSum)}else{H=0}
+if (this.indexOf('h') > -1){var h = this.split("h").join("8").match(/(8)/gim).map(Number).reduce(getSum)}else{h=0}
+if (this.indexOf('I') > -1){var I = this.split("I").join("10").match(/(10)/gim).map(Number).reduce(getSum)}else{I=0}
+if (this.indexOf('i') > -1){var i = this.split("i").join("10").match(/(10)/gim).map(Number).reduce(getSum)}else{i=0}
+if (this.indexOf('J') > -1){var J = this.split("J").join("3").match(/(3)/gim).map(Number).reduce(getSum)}else{J=0}
+if (this.indexOf('j') > -1){var j = this.split("j").join("3").match(/(3)/gim).map(Number).reduce(getSum)}else{j=0}
+if (this.indexOf('K') > -1){var K = this.split("K").join("20").match(/(20)/gim).map(Number).reduce(getSum)}else{K=0}
+if (this.indexOf('k') > -1){var k = this.split("k").join("20").match(/(20)/gim).map(Number).reduce(getSum)}else{k=0}
+if (this.indexOf('L') > -1){var L = this.split("L").join("30").match(/(30)/gim).map(Number).reduce(getSum)}else{L=0}
+if (this.indexOf('l') > -1){var l = this.split("l").join("30").match(/(30)/gim).map(Number).reduce(getSum)}else{l=0}
+if (this.indexOf('M') > -1){var M = this.split("M").join("40").match(/(40)/gim).map(Number).reduce(getSum)}else{M=0}
+if (this.indexOf('m') > -1){var m = this.split("m").join("40").match(/(40)/gim).map(Number).reduce(getSum)}else{m=0}
+if (this.indexOf('N') > -1){var N = this.split("N").join("50").match(/(50)/gim).map(Number).reduce(getSum)}else{N=0}
+if (this.indexOf('n') > -1){var n = this.split("n").join("50").match(/(50)/gim).map(Number).reduce(getSum)}else{n=0}
+if (this.indexOf('O') > -1){var O = this.split("O").join("70").match(/(70)/gim).map(Number).reduce(getSum)}else{O=0}
+if (this.indexOf('o') > -1){var o = this.split("o").join("70").match(/(70)/gim).map(Number).reduce(getSum)}else{o=0}
+if (this.indexOf('P') > -1){var P = this.split("P").join("87").match(/(87)/gim).map(Number).reduce(getSum)}else{P=0}
+if (this.indexOf('p') > -1){var p = this.split("p").join("87").match(/(87)/gim).map(Number).reduce(getSum)}else{p=0}
+if (this.indexOf('Q') > -1){var Q = this.split("Q").join("100").match(/(100)/gim).map(Number).reduce(getSum)}else{Q=0}
+if (this.indexOf('q') > -1){var q = this.split("q").join("100").match(/(100)/gim).map(Number).reduce(getSum)}else{q=0}
+if (this.indexOf('R') > -1){var R = this.split("R").join("200").match(/(200)/gim).map(Number).reduce(getSum)}else{R=0}
+if (this.indexOf('r') > -1){var r = this.split("r").join("200").match(/(200)/gim).map(Number).reduce(getSum)}else{r=0}
+if (this.indexOf('S') > -1){var S = this.split("S").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{S=0}
+if (this.indexOf('s') > -1){var s = this.split("s").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{s=0}
+if (this.indexOf('T') > -1){var T = this.split("T").join("400").match(/(400)/gim).map(Number).reduce(getSum)}else{T=0}
+if (this.indexOf('t') > -1){var t = this.split("t").join("400").match(/(400)/gim).map(Number).reduce(getSum)}else{t=0}
+if (this.indexOf('U') > -1){var U = this.split("U").join("800").match(/(800)/gim).map(Number).reduce(getSum)}else{U=0}
+if (this.indexOf('u') > -1){var u = this.split("u").join("800").match(/(800)/gim).map(Number).reduce(getSum)}else{u=0}
+if (this.indexOf('V') > -1){var V = this.split("V").join("6").match(/(6)/gim).map(Number).reduce(getSum)}else{V=0}
+if (this.indexOf('v') > -1){var v = this.split("v").join("6").match(/(6)/gim).map(Number).reduce(getSum)}else{v=0}
+if (this.indexOf('W') > -1){var W = this.split("W").join("800").match(/(800)/gim).map(Number).reduce(getSum)}else{W=0}
+if (this.indexOf('w') > -1){var w = this.split("w").join("800").match(/(800)/gim).map(Number).reduce(getSum)}else{w=0}
+if (this.indexOf('X') > -1){var X = this.split("X").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{X=0}
+if (this.indexOf('x') > -1){var x = this.split("x").join("60").match(/(60)/gim).map(Number).reduce(getSum)}else{x=0}
+if (this.indexOf('Y') > -1){var Y = this.split("Y").join("10").match(/(10)/gim).map(Number).reduce(getSum)}else{Y=0}
+if (this.indexOf('y') > -1){var y = this.split("y").join("10").match(/(10)/gim).map(Number).reduce(getSum)}else{y=0}
+if (this.indexOf('Z') > -1){var Z = this.split("Z").join("7").match(/(7)/gim).map(Number).reduce(getSum)}else{Z=0}
+if (this.indexOf('z') > -1){var z = this.split("z").join("7").match(/(7)/gim).map(Number).reduce(getSum)}else{z=0}
 
-    return ا+آ+ب+پ+ج+چ+د+ه+و+ز+ژ+ح+ط+ی+ي+ک+گ+ل+م+ن+س+ع+ف+ص+ق+ر+ش+ت+ث+خ+ذ+ض+ظ+غ
+    
+    return A+a+B+b+C+c+D+d+E+e+F+f+G+g+H+h+I+i+J+j+K+k+L+l+M+m+N+n+O+o+P+p+Q+q+R+r+S+s+T+t+U+u+V+v+W+w+X+x+Y+y+Z+z+ا+آ+ب+پ+ج+چ+د+ه+و+ز+ژ+ح+ط+ی+ي+ک+گ+ل+م+ن+س+ع+ف+ص+ق+ر+ش+ت+ث+خ+ذ+ض+ظ+غ
 };
 
 Ask();
 
 function Ask() {
-    var text = prompt("Please enter your Arabic text");
+    var text = prompt("Please enter your Arabic or English text");
     if (text != null) {
     alert("Gematrical value is: " + text.abjad())
     document.getElementById("result").innerHTML = text.abjad();
